ae0246fc3e86e0e09152175130fbd6d92b9bb483
Neto Kokay <netokokay@gmail.com>
2022-08-22 12:23:52 -0300

    Atualização 1.0

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const axios = require("axios");
 const btn = document.querySelector("#send");
-//let name = document.getElementById("#nomeChar").value;
+//const nomeDoChar = document.querySelector("#nickName")
 
-
-async function start() {
+async function start(value) {
     try {
 
         const url = `https://api.tibiadata.com/v3/character/${value}`
         const resposta = await axios.get(url);
-        const info = resposta.data.character;
+        const nick = resposta.data.characters.character.name;
         const level = resposta.data.characters.character.level;
-        console.log(level)
-        console.log(info)
+        console.log(`Nome do Char: ${nick}`)
+        console.log(`Level: ${level}`)
+        console.log(resposta.data)
+        document.getElementById("nickName").value = resposta.data.characters.character.name;
+        document.getElementById("levelInput").value = level;
+        document.getElementById("vocInput").value = resposta.data.characters.character.vocation;
+        const sex = resposta.data.characters.character.sex;
+        if (sex === 'female'){
+            document.getElementById("sexInput").value = 'Feminino';
+        } else{
+            document.getElementById("sexInput").value = 'Masculino';
+        }
+        document.getElementById("mundoInput").value = resposta.data.characters.character.world;
+        document.getElementById("cityInput").value = resposta.data.characters.character.residence;
+        document.getElementById("statusInput").value = resposta.data.characters.character.account_status;
+
+
+
+
 } catch (error) {
+    console.log(error)
         console.log("Erro na conexão com API")
     }
 }
@@ -25,6 +41,17 @@ btn.addEventListener("click",function(e){
 
     const value = name.value;
 
-    console.log(value)
-    start();
+    console.log(`O nome digitado foi: ${value}`)
+
+    start(value);
 })
+
+document.addEventListener('keypress', function(e){
+    if(e.which == 13){
+        const name = document.querySelector("#nomeChar");
+    
+       
+       const value = name.value;
+       start(value);
+    }
+ }, false);
